cf8633926ba52ef0fb8df0b8a249eba898534719
pilauria <ptr.lauria@gmail.com>
2021-10-12 12:14:53 +0200

    add readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+Simple app to learn how to Connect to an API with JavaScript
+
+source:
+
+[tania tarascia](https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/)
---
+++ scripts.js
@@ -0,0 +1,44 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.jpg';
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+// Replace ./data.json with your JSON feed
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    // Work with JSON data here
+    data.forEach(movie => {
+      // Create a div with a class='card'
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      // Create an h1 and set the text content to the film's title
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      // Create a p and set the text content to the film's description
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300); // Limit to 300 chars
+      p.textContent = `${movie.description}...`; // End with an ellipses
+
+      // Append the cards to the container element
+      container.appendChild(card);
+
+      // Each card will contain an h1 and a p
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  })
+  .catch(err => {
+    err = document.createElement('marquee');
+    err.textContent = `Gah, it's not working!`;
+    app.appendChild(err);
+  });
